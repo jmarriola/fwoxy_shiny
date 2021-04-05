17eaf014b00112e137208679ee763cd2cc53ff3e
Jill Arriola <jillbert79@gmail.com>
2021-04-05 13:43:47 -0400

    Creation of fwoxy_shiny

---
+++ app.R
@@ -0,0 +1,21 @@
+# Shiny app to run fwoxy.R
+# 2021-04-05 Jill Arriola
+# fwoxy.R is housed at https://github.com/jmarriola/fwoxy
+#
+#
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
