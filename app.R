8d73c79570ba9cb4835e7c4b9072f896458558f8
Jill Arriola <jillbert79@gmail.com>
2021-04-05 15:16:38 -0400

    Inital build of app

--- app.R
+++ app.R
@@ -7,7 +7,22 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+   titlePanel("Forward Oxygen Model"),
    
+   sidebarLayout(
+     sidebarPanel(
+       h3("Parameters"),
+     
+       sliderInput("a_param", h4("Light efficiency"),
+                        min = 0.1, max = 1.0, value = 0.2),
+       
+       sliderInput("er_param", h4("Ecosystem respiration"),
+                        min = 0, max = 80, value = 20)
+     ),
+   
+   mainPanel("Output")
+  
+   )
 )
 
 # Define server logic required to draw a histogram
